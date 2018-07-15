9e4a0fa7a57d4d4d2f18382c483220db3068b7ed
Jarosław Bagnicki <jarek@bagnicki.net>
2018-07-15 05:52:07 +0200

    Add webpack loaders, plugins. Try write script to compile scss (fail - wrong sourceMap paths).

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// const devMode = process.env.NODE_ENV !== 'production';
+// console.log('NODE_ENV: ', process.env.NODE_ENV);
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './sass/main.scss',
+  module: {
+      rules: [{
+          test: /\.scss$/,
+          use: [
+            // MiniCssExtractPlugin.loader,
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'sass-loader',
+              options: { 
+                sourceMap: true,
+                omitSourceMapUrl: true 
+              },
+            },
+          ]
+      }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css", // [name].css or  [hash].css or [contenthash].css
+      
+    })
+  ]
+};
